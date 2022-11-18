9a2fa575f3bef938c1c85369bae116ef0c704ee6
Delashoo <mwadimesammy4@gmail.com>
2022-11-18 10:26:39 +0300

    Update HeyFundMe.sol

--- HeyFundMe.sol
+++ HeyFundMe.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     
     mapping (address => uint256) AddressToAmountFunded;
@@ -10,6 +12,7 @@ contract FundMe {
     }
     
     function GetVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface();
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        return priceFeed.version();
     }
 }
