311c7c1becf73b45dbb72b97af47ad9f67134de0
Delashoo <mwadimesammy4@gmail.com>
2022-11-19 16:37:02 +0300

    Update HeyFundMe.sol

--- HeyFundMe.sol
+++ HeyFundMe.sol
@@ -15,4 +15,16 @@ contract FundMe {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
         return priceFeed.version();
     }
+
+    function GetPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound)
+        = priceFeed.latestRoundData();
+        return uint256(answer);
+    }
 }
